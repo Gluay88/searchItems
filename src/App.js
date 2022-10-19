4eb51f30a556061f7350053b5a87e663c25ebb00
gluay88 <p.wantagaew@gmail.comm>
2022-10-18 19:32:02 -0600

    hiya

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef, useMemo } from "react";
 
 function App() {
+  // Derived State
+  const [items, setItems] = useState([]);
+  const [query, setQuery] = useState("");
+
+  const inputRef = useRef();
+
+  const filteredItems = useMemo(
+    () =>
+      items.filter((item) => {
+        return item.toLowerCase().includes(query.toLocaleLowerCase());
+      }),
+    [items, query]
+  );
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const value = inputRef.current.value;
+    if (value === "") return;
+    setItems((prev) => {
+      return [...prev, value];
+    });
+    inputRef.current.value = "";
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Search:
+      <br />
+      <input
+        value={query}
+        type="search"
+        onChange={(e) => setQuery(e.target.value)}
+      />
+      <br />
+      <br />
+      <form onSubmit={onSubmit}>
+        New Items:
+        <br />
+        <input type="text" ref={inputRef} />
+        <button type="submit">Add</button>
+      </form>
+      <h3>Items:</h3>
+      {filteredItems.map((item, index) => (
+        <div key="item.id">{item}</div>
+      ))}
     </div>
   );
 }
